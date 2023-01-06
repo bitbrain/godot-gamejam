9f0bb0f19a4e365e33959152507d70b88f7bb2c1
Miguel Gonzalez Sanchez <m.gonzalez.sanchez.1990@gmail.com>
2023-01-06 21:53:16 +0000

    :sparkles: pause menu

--- godot/scenes/IngameScene.gd
+++ godot/scenes/IngameScene.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
-@onready var overlay = %FadeOverlay
+@onready var fade_overlay = %FadeOverlay
+@onready var pause_overlay = %PauseOverlay
 
 func _ready():
-	overlay.visible = true
+	fade_overlay.visible = true
+
+func _input(event):
+	if event.is_action_pressed("pause") and not pause_overlay.visible:
+		get_viewport().set_input_as_handled()
+		get_tree().paused = true
+		pause_overlay.grab_focus()
+		pause_overlay.visible = true
